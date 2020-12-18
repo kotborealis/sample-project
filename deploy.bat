7f66de709a15f3920f47254b1843881d309ddf1d
kotborealis <kotborealis@gmail.com>
2020-12-18 13:27:17 +0300

    :sparkles: Added deploy script

---
+++ deploy.bat
@@ -0,0 +1,8 @@
+C:\Qt\5.15.1\msvc2019_64\bin\windeployqt.exe release\sample-project.exe
+copy /y /b c:\windows\system32\api-ms-win-core-file-l1-2-0.dll release\
+copy /y /b c:\windows\system32\api-ms-win-core-file-l2-1-0.dll release\
+copy /y /b c:\windows\system32\api-ms-win-core-localization-l1-2-0.dll release\
+copy /y /b c:\windows\system32\api-ms-win-core-processthreads-l1-1-1.dll release\
+copy /y /b c:\windows\system32\api-ms-win-core-synch-l1-2-0.dll release\
+copy /y /b c:\windows\system32\api-ms-win-core-timezone-l1-1-0.dll release\
+copy /y /b c:\windows\system32\ucrtbased.dll release\
